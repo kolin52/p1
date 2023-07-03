74fb451ebdb1a6e718aae1c9f99cc7c8c3cae86c
kolin52 <138013058+kolin52@users.noreply.github.com>
2023-07-04 00:40:48 +0300

    Add files via upload

---
+++ zadacha8.py
@@ -0,0 +1,8 @@
+dal_uspor = 5
+vis_uspor = 8
+# izchislyavame liceto na usporednika 
+lice_uspor = int(dal_uspor * vis_uspor)
+# izchislyavame perimetara na usporednika 
+perim_uspor = int(2 * dal_uspor + 2 * vis_uspor)
+print(lice_uspor)
+print(perim_uspor)
