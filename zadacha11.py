76da553c9f620e9423d773aff2c4fbbc49fd3fd1
kolin52 <138013058+kolin52@users.noreply.github.com>
2023-07-04 00:52:25 +0300

    Add files via upload

---
+++ zadacha11.py
@@ -0,0 +1,5 @@
+# s sa inchovete, koito potrebitelya zadava
+s = float(input('Zadajte inchovete, koito da presmetnete v santimetri: '))
+# napisvame formulata za izchislenie na santimetrite
+s1 = float(s * 2.54)
+print('Transformiranite inchove v zantimetri sa: ', s1)
