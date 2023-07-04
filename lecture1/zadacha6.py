c3eed80e774bb9f8fb07384b95bd205859325b9d
kolin52 <138013058+kolin52@users.noreply.github.com>
2023-07-04 08:38:35 +0300

    Rename zadacha6.py to lecture1/zadacha6.py

---
+++ lecture1/zadacha6.py
@@ -0,0 +1,6 @@
+name = 'Stoyan'
+familia = 'Ivanov'
+age = '45'
+pol = 'Man'
+unikalen_num = 27560001
+print("Our worker's name is", name, familia, ", he is", age, "years old ot pol", pol, "s unikalen nomer", unikalen_num)
--- zadacha6.py
+++
@@ -1,6 +0,0 @@
-name = 'Stoyan'
-familia = 'Ivanov'
-age = '45'
-pol = 'Man'
-unikalen_num = 27560001
-print("Our worker's name is", name, familia, ", he is", age, "years old ot pol", pol, "s unikalen nomer", unikalen_num)
