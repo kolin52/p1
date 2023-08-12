989227c3f59e688c2ca8232e7f504697a65b34f6
kolin52 <138013058+kolin52@users.noreply.github.com>
2023-08-12 19:01:36 +0300

    Update l7-7.py

--- exercise/l7-7.py
+++ exercise/l7-7.py
@@ -28,7 +28,23 @@ for x in b:
     if len(x) > len(longest):
         longest = x
 print(longest)
-                                
+
+#Second
+a = [2,1,1,2,3,3,2,2,2,1]
+b = []
+i = 0
+while i < len(a) - 1:
+    b = []
+    consec = []
+    while a[i] == a[i + 1]:
+        consec.append(a[i])
+        i += 1
+    consec.append(a[i])
+    b.append(consec)
+    i += 1
+    if len(consec) > len(b):
+        b = consec
+print(consec)
 
 
 
